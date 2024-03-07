77b33032b051d5c8c060d62846f5db44cc6406e8
samuelvictorol <990905>
2024-03-06 23:49:14 -0300

    initial commit

---
+++ src/routes/router.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+
+// const skillsRouter = require('./products');
+
+// router.use('/', skillsRouter);
+
+module.exports = router;
---
+++ src/routes/skills.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+// const productController = require('../controllers/productController');
+
+// router.route('/products')
+//     .post((req, res) => productController.create(req, res))
+//     .get((req, res) => productController.getAllProducts(req, res));
+
+// router.delete('/products/:id', (req, res) => productController.removeProduct(req, res));
+
+module.exports = router;
